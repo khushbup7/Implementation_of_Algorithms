238f7a97d1d64fed808dd4bc8311b8624a4a0ce5
Khushbu <kxp153130@utdallas.edu>
2017-08-28 16:34:43 -0500

    TestGraph class

---
+++ SP1/src/cs6301/g39/TestGraph.java
@@ -0,0 +1,23 @@
+package cs6301.g39;
+
+import cs6301.g00.Graph;
+
+public class TestGraph {
+	
+	public static void main(String[] args) {
+		int n = 10;
+		Graph g = new Graph(n);
+		g.addEdge(g.getVertex(1), g.getVertex(2), 1);
+		g.addEdge(g.getVertex(1), g.getVertex(3), 1);
+		g.addEdge(g.getVertex(2), g.getVertex(4), 1);
+		g.addEdge(g.getVertex(2), g.getVertex(5), 1);
+		g.addEdge(g.getVertex(3), g.getVertex(6), 1);
+		g.addEdge(g.getVertex(3), g.getVertex(7), 1);
+		g.addEdge(g.getVertex(4), g.getVertex(8), 1);
+		g.addEdge(g.getVertex(4), g.getVertex(9), 1);
+		g.addEdge(g.getVertex(8), g.getVertex(10), 1);
+		System.out.println();
+	}
+	
+	
+}
