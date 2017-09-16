35169d8df3dd78cdfa170d18adff8238bdbdbfe2
VATSAL-PC\Vatsal <vatsalchilodiya@gmail.com>
2017-09-16 13:28:46 -0500

    Attempt-1 : only DFS

--- SP3/src/cs6301/g39/DFS.java
+++ SP3/src/cs6301/g39/DFS.java
@@ -1,6 +1,37 @@
 package cs6301.g39;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
+
+	public static final int INFINITY = Integer.MAX_VALUE;
+	
+	// Class to store information about a vertex in this algorithm
+	static class DFSVertex {
+		boolean seen;
+		DFS.DFSVertex parent;
+		int distance; // distance of vertex from source
+		int componentNo; // 
+		int top;
+		int fin;
+
+		DFSVertex(Graph.Vertex u) {
+			seen = false;
+			parent = null;
+			distance = INFINITY;
+			componentNo = -1;
+			top = -1;
+			fin = -1;
+			
+		}	
+	}
+	
+	int topNum;
+	int cNo;
+	int finTime;
+	LinkedList<Graph.Vertex> decFinList = new LinkedList<Graph.Vertex>();
+	
 	public DFS(Graph g) {
 		super(g);
 		node = new DFSVertex[g.size()];
@@ -8,56 +39,77 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 		for (Graph.Vertex u : g) {
 			node[u.getName()] = new DFSVertex(u);
 		}
-		// Set source to be at distance 0
-		// getVertex(src).distance = 0;
+		
+		topNum = g.size();
+		cNo = 0;
+		finTime = 0;
 	}
-
-	public static final int INFINITY = Integer.MAX_VALUE;
-
-	// Class to store information about a vertex in this algorithm
-	static class DFSVertex {
-		boolean seen;
-		Graph.Vertex parent;
-		int distance; // distance of vertex from source
-		int componentNo; // 
-
-		DFSVertex(Graph.Vertex u) {
-			seen = false;
-			parent = null;
-			distance = INFINITY;
-			componentNo = -1;
+	
+	void dfs(Iterator<Graph.Vertex> it) {
+		
+		while(it.hasNext()) {
+			Graph.Vertex u = it.next();
+			
+			if(!seen(u)) {
+				cNo++;
+				DFSVisit(u);
+			}
+		}
+	}
+	
+	void DFSVisit(Graph.Vertex u){
+		
+		DFSVertex dvU = getVertex(u);
+		dvU.seen = true;
+		dvU.distance = ++finTime;
+		dvU.componentNo = cNo;
+		
+		Iterator<Graph.Edge> outEdgeItr = u.iterator();
+		
+		while(outEdgeItr.hasNext()) {
+			Graph.Edge e = outEdgeItr.next();
+			Graph.Vertex v = e.otherEnd(u);
+			DFSVertex dvV = getVertex(v);
+			
+			if(!seen(v)){
+				dvV.parent = dvU;
+				DFSVisit(v);
+			}
 		}
 		
-		public boolean isSeen() {
-			return seen;
-		}
-
-		public void setSeen(boolean seen) {
-			this.seen = seen;
-		}
-
-		public Graph.Vertex getParent() {
-			return parent;
-		}
-
-		public void setParent(Graph.Vertex parent) {
-			this.parent = parent;
-		}
-
-		public int getDistance() {
-			return distance;
-		}
-
-		public void setDistance(int distance) {
-			this.distance = distance;
-		}
-
-		public int getComponentNo() {
-			return componentNo;
-		}
-
-		public void setComponentNo(int componentNo) {
-			this.componentNo = componentNo;
-		}
+		dvU.fin = ++finTime;
+		dvU.top = topNum--;
+		decFinList.addFirst(u);
 	}
+	
+		boolean seen(Graph.Vertex u) {
+		return getVertex(u).seen;
+	    }
+		
+		/*
+	    Graph.Vertex getParent(Graph.Vertex u) {
+		return getVertex(u).parent;
+	    }
+	    */
+
+	    int distance(Graph.Vertex u) {
+		return getVertex(u).distance;
+	    }
+	    
+	    void reinitialize() {
+	    	for(Graph.Vertex u: g) {
+	    	    DFSVertex bu = getVertex(u);
+	    	    bu.seen = false;
+	    	    bu.parent = null;
+	    	    bu.distance = INFINITY;
+	    	    bu.componentNo = -1;
+				bu.top = -1;
+				bu.fin = -1;
+	    	}
+	    	
+	    	topNum = g.size();
+			cNo = 0;
+			finTime = 0;
+			decFinList.removeAll(decFinList);
+	    }
 }
