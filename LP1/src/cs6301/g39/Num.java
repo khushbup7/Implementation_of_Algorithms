59531aa0f68d948bff91e0e04f5539c3b9086e82
Khushbu <kxp153130@utdallas.edu>
2017-09-16 20:32:52 -0500

    compareTo implementation

--- LP1/src/cs6301/g39/Num.java
+++ LP1/src/cs6301/g39/Num.java
@@ -59,11 +59,15 @@ public class Num  implements Comparable<Num> {
     		result.sign = true;
     	if(!a.sign && b.sign) {
     		b.sign = false;
-    		return subtract(a,b);
+    		result = subtract(a,b);
+    		b.sign = true;
+    		return result;
     	}
     	if(a.sign && !b.sign) {
     		a.sign = false;
-    		return subtract(b,a);
+    		result = subtract(b,a);
+    		a.sign = true;
+    		return result;
     	}
 
     	long carry = 0;
@@ -93,8 +97,9 @@ public class Num  implements Comparable<Num> {
     	}
     	
     	if(a.compareTo(b) < 0) {
-    		//a.sign = !a.sign;
+    		a.sign = !a.sign;
     		res = subtract(b, a);
+    		a.sign = !a.sign;
     		res.sign = true;
     		return res;
     	}
@@ -183,27 +188,36 @@ public class Num  implements Comparable<Num> {
     }
     // compare "this" to "other": return +1 if this is greater, 0 if equal, -1 otherwise
     public int compareTo(Num other) {
-    	if(this.size < other.size || (this.sign && !other.sign))
+    	int mag = compareMagnitude(this, other);
+    	int sign = this.sign ? -1 : 1;
+    	int othersign = other.sign ? -1 : 1;
+    	
+    	
+    	 return (sign == othersign
+    			   ? sign * mag
+    			   : (sign > othersign ? 1 : -1));
+    }
+    
+    
+    int compareMagnitude(Num a, Num b) {
+    	if(a.size < b.size)
     		return -1;
-    	if(this.size > other.size || (!this.sign && other.sign))
+    	if(a.size > b.size)    		
     		return 1;
     	
-    	int res = 0;
-    	Iterator<Long> itA = this.value.iterator();
-    	Iterator<Long> itB = other.value.iterator();
+    	Iterator<Long> itA = a.value.iterator();
+    	Iterator<Long> itB = b.value.iterator();
     	long valueA, valueB;
     	
     	while(itA.hasNext() && itB.hasNext()) {
     		valueA = next(itA);
     		valueB = next(itB);
     		if(valueA < valueB)
-    			res = -1;
+    			return -1;
     		else if(valueA > valueB)
-    			res = 1;
+    			return 1;
     	}
-    	if(this.sign && other.sign)
-    		return res * -1;
-	return res;
+    	return 0;
     }
     
     // Output using the format "base: elements of list ..."
