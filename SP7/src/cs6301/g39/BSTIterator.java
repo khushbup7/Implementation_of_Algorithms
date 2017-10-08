4c0a646b97bee169eb96904bd943e58460d10ab8
Khushbu <kxp153130@utdallas.edu>
2017-10-08 14:16:31 -0500

    Fixed BST Iterator

--- SP7/src/cs6301/g39/BSTIterator.java
+++ SP7/src/cs6301/g39/BSTIterator.java
@@ -4,14 +4,14 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class BSTIterator<T extends Comparable<? super T>> implements Iterator<T> {
-	
+
 	Stack<BST.Entry<T>> itrStack;
 	BST.Entry<T> current;
-	
-	BSTIterator(BST<T> bst){
+
+	BSTIterator(BST<T> bst) {
 		itrStack = new Stack<BST.Entry<T>>();
 		current = bst.root;
-		while(current!=null) {
+		while (current != null) {
 			itrStack.push(current);
 			current = current.left;
 		}
@@ -19,16 +19,16 @@ public class BSTIterator<T extends Comparable<? super T>> implements Iterator<T>
 
 	@Override
 	public boolean hasNext() {
-		return current!=null && !itrStack.isEmpty();
+		return !itrStack.isEmpty();
 	}
 
 	@Override
 	public T next() {
 		BST.Entry<T> nextNode = null;
-		if(current !=null && !itrStack.isEmpty()) {
+		if (!itrStack.isEmpty()) {
 			nextNode = itrStack.pop();
 			current = nextNode.right;
-			while(current!=null) {
+			while (current != null) {
 				itrStack.push(current);
 				current = current.left;
 			}
