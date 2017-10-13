408cd1b44424df5289152b1be29d9433e3fd2965
VATSAL-PC\Vatsal <vatsalchilodiya@gmail.com>
2017-10-13 16:33:14 -0500

    Sp7 - RedBlack tress Starter code

--- SP7/src/cs6301/g39/RedBlackTree.java
+++ SP7/src/cs6301/g39/RedBlackTree.java
@@ -17,5 +17,60 @@ public class RedBlackTree<T extends Comparable<? super T>> extends BST<T> {
     RedBlackTree() {
 	super();
     }
+    
+    public boolean add(T x) {
+    	boolean b = super.add(x);
+    	if(!b)
+    		return b;
+    	else {
+    		RedBlackTree.Entry<T> parent = (RedBlackTree.Entry<T>)stack.peek();
+    		if(!parent.isRed)  //New node is craeted red, so if parent is black, nothing needs to be changed
+    			return true;
+    		else {//parent is red, tree needs to be repaired
+    			RedBlackTree.Entry<T> t = (RedBlackTree.Entry<T>) (parent.left.element.compareTo(x) == 0 ? parent.left : parent.right);
+    			repair(t);
+    			//TODO color the root black after every repair
+    		}
+    	}
+		return false;
+    	
+    }
+    
+    public void repair(RedBlackTree.Entry<T> t) {
+    	RedBlackTree.Entry<T> parent = (RedBlackTree.Entry<T>)stack.pop();
+    	RedBlackTree.Entry<T> grandParent = (RedBlackTree.Entry<T>)stack.pop();
+    	RedBlackTree.Entry<T> uncle = (RedBlackTree.Entry<T>)(grandParent.left == parent ? grandParent.right : grandParent.left);
+    	
+    	if(parent == root) //root is red now, but is colored black by add after repair
+    		return;
+    	
+    	while(t.isRed) {
+    		if(t == root || parent == root || !parent.isRed)
+    			return;
+    		
+    		if(uncle.isRed) { //if u_t is red : case-1
+    			parent.isRed = false;
+    			uncle.isRed = false;
+    			grandParent.isRed = true;
+    			t = grandParent;
+    			continue;
+    		}
+    		else {  //case-2 & 3 : uncle is black
+    			if(parent == grandParent.left && t == parent.left) {//LL - 2a
+    				
+    			}
+    			else if(parent == grandParent.right && t == parent.right) { //RR - 2b
+    				
+    			}
+    			else if(parent == grandParent.left && t == parent.right) { //LR - 3a
+    				
+    			}
+    			else {//RL - 3b
+    				
+    			}
+    		}
+    	}
+    	
+    }
 }
 
